43017a6775bae952b41d945b9bf02de05a10b560
DenDenDeniska <deniskostrov24@gmail.com>
2024-04-08 00:15:12 +0200

    firts_commit

---
+++ exercise.py
@@ -0,0 +1,78 @@
+from collections import UserDict
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self) -> str:
+        return str(self.value)
+
+class Name(Field):
+    # реалізація класу
+    def __init__(self, value):
+        super().__init__(value)
+
+class Phone(Field):
+    # реалізація класу
+		pass
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones: list = []
+
+    # реалізація класу
+    def add_phone(self, number):
+        self.phones.append(number)
+
+    def __str__(self) -> str:
+        return f"Contact name: {self.name.value}, phones: {'; '.join(p for p in self.phones)}"
+
+class AddressBook(UserDict):
+    # реалізація класу
+    def add_record(self, name):
+        self.data.update({name.name: name})
+
+    def find(self, name):
+        return self.data.get(name)
+
+    
+    def edit_phone(self, old_phone, new_phone):
+        result = next(note for note in self.data if note.value == name)
+        self.data.update(result.phones[old_phone], new_phone)
+
+# Створення нової адресної книги
+book = AddressBook()
+
+# Створення запису для John
+john_record = Record("John")
+
+john_record.add_phone("1234567890")
+john_record.add_phone("5555555555")
+
+
+# Додавання запису John до адресної книги
+book.add_record(john_record)
+
+# Створення та додавання нового запису для Jane
+jane_record = Record("Jane")
+jane_record.add_phone("9876543210")
+book.add_record(jane_record)
+
+# Виведення всіх записів у книзі
+for name, record in book.data.items():
+    print(record)
+
+# Знаходження та редагування телефону для John
+john = book.find("John")
+
+john.edit_phone("1234567890", "1112223333")
+
+print(john)  # Виведення: Contact name: John, phones: 1112223333; 5555555555
+
+# Пошук конкретного телефону у записі John
+found_phone = john.find_phone("5555555555")
+print(f"{john.name}: {found_phone}")  # Виведення: 5555555555
+
+# Видалення запису Jane
+book.delete("Jane")
