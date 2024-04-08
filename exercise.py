a5e9b35dcbd3ff7ac6e506052659a38dcc606796
DenDenDeniska <deniskostrov24@gmail.com>
2024-04-08 21:58:21 +0200

    correct_mistake

--- exercise.py
+++ exercise.py
@@ -1,4 +1,5 @@
 from collections import UserDict
+from hmac import new
 
 class Field:
     def __init__(self, value):
@@ -14,7 +15,11 @@ class Name(Field):
 
 class Phone(Field):
     # реалізація класу
-		pass
+    def __init__(self, value):
+        if len(value) == 10 and value.isdigit():
+            super().__init__(value)
+        else:
+            raise ValueError("Телефон неверного формата")
 
 class Record:
     def __init__(self, name):
@@ -23,18 +28,33 @@ class Record:
 
     # реалізація класу
     def add_phone(self, number):
-        self.phones.append(number)
+        self.phones.append(Phone(number))
+    
+    def edit_phone(self, old_number, new_number):
+        number = next(num for num in self.phones if num.value == old_number)
+        self.phones[self.phones.index(number)] = Phone(new_number)
+
+    def find_phone(self, number):
+        number = next(num for num in self.phones if num.value == number)
+        return self.phones[self.phones.index(number)]
+
+    def remove_phone(self, number):
+        number = next(num for num in self.phones if num.value == number)
+        del self.phones[self.phones.index(number)]
 
     def __str__(self) -> str:
-        return f"Contact name: {self.name.value}, phones: {'; '.join(p for p in self.phones)}"
+        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
 
 class AddressBook(UserDict):
     # реалізація класу
     def add_record(self, name):
-        self.data.update({name.name: name})
+        self.data.update({name.name.value: name})
 
     def find(self, name):
         return self.data.get(name)
+
+    def delete(self, name):
+        del self.data[name]
 
 
 # Створення нової адресної книги
@@ -61,9 +81,8 @@ for name, record in book.data.items():
 
 # Знаходження та редагування телефону для John
 john = book.find("John")
-
+print(john)
 john.edit_phone("1234567890", "1112223333")
-
 print(john)  # Виведення: Contact name: John, phones: 1112223333; 5555555555
 
 # Пошук конкретного телефону у записі John
